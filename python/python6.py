609eff1ac97325419519a58cd5aac9bf65a92032
sofiprovvi <sofiaprovvisionato7@gmail.com>
2024-05-21 21:08:31 -0300

    python6 ej3 y ej 4

--- python/python6.py
+++ python/python6.py
@@ -25,7 +25,6 @@ def perimetro () -> float:
 
 
 
-
 #EJERCICIO 2:
 
 #2.1)
@@ -39,5 +38,64 @@ def raiz_cuadrada_de (numero: float) -> float:
 #2.3)
 def fahrenheit_a_celsius (t: float) -> float:
     return ((t-32)*5)/9
+
+#2.4)
+def imprimir_dos_veces (estribillo: str):
+    return 2*(estribillo + " ")
+
+#2.5)
+def es_multiplo_de (n: int, m: int) -> bool:
+    if (n/m) == math.ceil(n/m): es_multiplo_de =True
+    else: es_multiplo_de =False
+    return es_multiplo_de       
+
+#2.6)
+def es_par (numero: int) -> bool:
+    if es_multiplo_de (numero, 2): es_par =True
+    else: es_par =False
+    return es_par
+
+#2.7)
+def cantidad_de_pizzas (comensales: int, min_cant_de_porciones: int) -> int:
+    if round((comensales*min_cant_de_porciones)/8) == (comensales*min_cant_de_porciones)/8: cantidad_de_pizzas =(comensales*min_cant_de_porciones)/8
+    else: cantidad_de_pizzas =round((comensales*min_cant_de_porciones)/8)+1
+    return cantidad_de_pizzas
+
+
+#EJERCICIO 3:
+
+#3.1)
+def alguno_es_0 (numero1: int, numero2: int) -> bool:
+    return (numero1 ==0) or (numero2 ==0) 
+
+#3.2)
+def ambos_son_0 (numero1: int, numero2: int) -> bool:
+    return (numero1 ==0) and (numero2 ==0) 
+
+#3.3)
+def es_nombre_largo (nombre: str) -> bool:
+    return ((len(nombre) >= 3)) and ((len(nombre)) <=8) 
+
+#3.4)
+def es_bisiesto (año: int) -> bool:
+    return (es_multiplo_de (año,400)) or (es_multiplo_de (año,4) and (not (es_multiplo_de (año,400))))
+
+
+
+#EJERCICIO 4:
+
+def peso_pino (metros: float) -> float:
+    if metros<=3: peso_pino=metros*100*3
+    elif metros>3: peso_pino =900 + (metros-3)*100*2
+    return peso_pino
+
+def es_peso_util (peso: float) -> bool:
+    return (peso>=400) and (peso<=1000)
+
+def sirve_pino (metros: float) -> bool:
+    return (metros>=(1.33)) and (metros<=(3.5))
+
+def sirve_pino2 (metros: float) -> bool:
+    return (peso_pino (metros)>=400) and (peso_pino (metros)<=1000)
 
 
