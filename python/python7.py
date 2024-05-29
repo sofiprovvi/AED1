84ef7381f44bbb38a5e604438f68632282f4e90d
sofiprovvi <sofiaprovvisionato7@gmail.com>
2024-05-29 20:43:21 -0300

    ej 1 y 2 guia 7

--- python/python7.py
+++ python/python7.py
@@ -207,3 +207,103 @@ def vocales2 (palabra: str) -> bool:
 
     return mas_de_tres_vocales
 
+
+
+#Ejercicio 2:
+
+#2.1)
+def quitarPares1 (numeros: list [int]) -> list[int]:
+    for i in range(0,len(numeros)-1,1):
+        if numeros[i]%2==0:
+            numeros[i]=0
+        else:
+            numeros[i]=numeros[i]   
+    return numeros         
+
+enteros1: list = [1,2,3,4,5,6,7]
+print("quitarPares1:")
+print("--antes: " + str(enteros1))
+quitarPares1(enteros1)
+print("--después: " + str(enteros1))
+
+
+#2.2)
+def quitarPares2 (numeros: list [int]) -> list[int]:
+    reemplazar_por_cero: list[int] = []
+    for n in numeros:
+        if n%2==0:
+            reemplazar_por_cero=[0]
+        else:
+            reemplazar_por_cero=[n]  
+    return reemplazar_por_cero 
+
+enteros2: list = [1,2,3,4,5,6,7]
+print("quitarPares2:")
+print("--antes: " + str(enteros2))
+quitarPares2(enteros2)
+print("--después: " + str(enteros2))
+
+
+#2.4)
+def elimina_vocales1 (palabra: list[chr]) -> list[chr]:
+    nueva_palabra: list[chr] = []
+    for char in palabra:
+        if char in 'aeiouAEIOU': 
+            nueva_palabra += ['_']
+        elif (char !='a') and (char !='e') and (char !='i') and (char != 'o') and (char != 'u'): 
+            nueva_palabra += char
+    return nueva_palabra           
+
+palabra1: str = "taylor swift"
+print("elimina_vocales1")
+print("--antes: " + palabra1)
+print("--elimina_vocales: " + str(elimina_vocales1 (palabra1)))
+print("--después: " + palabra1)
+
+
+#2.3)
+def elimina_vocales2 (palabra: list[chr]) -> list[chr]:
+    nueva_palabra: list[chr] = []
+    for char in palabra:
+        if (char =='a') or (char =='e') or (char =='i') or (char == 'o') or (char == 'u'): 
+            nueva_palabra = nueva_palabra
+        elif (char !='a') and (char !='e') and (char !='i') and (char != 'o') and (char != 'u'): 
+            nueva_palabra += char
+    return nueva_palabra        
+
+palabra2: list[chr] = "taylor swift"
+print("elimina_vocales2")
+print("--antes: " + palabra2)
+elimina_vocales2 (palabra2)
+print("--después: " + palabra2)
+
+
+#2.5)
+def da_vuelta_str (palabra: list [chr]) -> list[chr]:
+    al_reves: list[chr] = []
+    for i in range(0,len(palabra)-1,1):
+        al_reves += [palabra[len(palabra)-i-1]] 
+    return al_reves + [palabra[0]]
+
+palabra3: str = "roma"
+print("da_vuelta_str:")
+print("--antes: " + palabra3)
+print("--da_vuelta_str: " + str(da_vuelta_str(palabra3)))
+print("--después: " + palabra3)
+
+
+
+#2.6)
+def eliminar_repetidos (palabra: list[chr]) -> list[chr]:
+    palabrita = []
+    sin_repetir: list[chr] = []
+    for i in range(0,len(palabra)-1,1):
+        if palabra[i] not in palabrita:
+           palabrita += palabra[i]
+    return palabrita                           
+
+palabra4: str = "odontología"
+print("eliminar_repetidos:")
+print("--antes: " + palabra4)
+print("--da_vuelta_str: " + str(eliminar_repetidos(palabra4)))
+print("--después: " + palabra4)
