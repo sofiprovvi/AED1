00ad9d34e14b9d6df69f319fc66236ed7c1d8615
ManuelGUrbanov <manuelgurbanov@gmail.com>
2024-06-03 13:52:14 -0300

    subo algo d la 8

--- python/python8.py
+++ python/python8.py
@@ -4,20 +4,35 @@
 #EJERCICIO 1:
 
 #1.1)
-def contar_lineas (nombre_archivo:str):
+def contar_lineas (nombre_archivo: str) -> int:
    f = open(nombre_archivo)
    res = 0
    for linea in f.readlines():
        res += 1
    f.close()
-   return res    
+   return res     
 
-def contar_lineas2 (nombre_archivo:str):
-   f = open(nombre_archivo)
-   res = 0
-   for linea in f.readlines():
-       res += 1
-   f.close()
-   return res       
+
+#1.2) #no terminé tdv
+def pertenece (linea: list[chr], palabra: str) -> bool:
+    lo_encontre: bool = False
+    i: int = 0
+    for texto in linea:
+        while i<len(palabra) and not (lo_encontre):
+             if palabra[i] == texto:
+                lo_encontre = True
+    return lo_encontre       
+     
+
+def existe_palabra (palabra : str, nombre_archivo : str) -> bool:
+    f = open(nombre_archivo) 
+    existe: bool = False 
+    for linea in f.readlines():
+        for p in linea:
+            if pertenece(p, palabra):
+                existe = True      
+    f.close()       
+    return existe
+        
 
 
