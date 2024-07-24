2b5ccd3993c2546bedca3c31f8300b735b31d0ad
sofiprovvi <sofiaprovvisionato7@gmail.com>
2024-07-24 16:58:30 -0300

    corrijo 1er parcial haskell

--- haskell/1erparcial.hs
+++ haskell/1erparcial.hs
@@ -2,6 +2,7 @@ module SolucionT2 where
 
 --EJERCICIO 1:
 generarStock :: [[Char]] -> [([Char],Int)]
+generarStock [] = []
 generarStock [x] = [(x , cantidadDeApariciones x [x])] 
 generarStock (x:xs) = eliminarRepetidos ([(x, cantidadDeApariciones x (x:xs))] ++ generarStock (xs))
 
@@ -13,9 +14,9 @@ cantidadDeApariciones c (x:xs) | c==x =1 + cantidadDeApariciones c (xs)
 
 eliminarRepetidos :: [([Char],Int)] -> [([Char],Int)]
 eliminarRepetidos [x] = [x]
-eliminarRepetidos (x:y:[]) | x==y =[x]
+eliminarRepetidos (x:y:[]) | (fst x)== (fst y) =[x]
                            | otherwise =[x]++[y]
-eliminarRepetidos (x:y:xs) | x==y = eliminarRepetidos (y:xs)   
+eliminarRepetidos (x:y:xs) | (fst x)==(fst y) = eliminarRepetidos (x:xs)   
                            | otherwise = [x] ++ eliminarRepetidos (quitarTodos x (y:xs))  
 
 
@@ -31,6 +32,7 @@ quitarTodos c (x:xs) | fst c==fst x = quitarTodos c (xs)
 
 --EJERCICIO 2:
 stockDeProducto :: [([Char],Int)] -> [Char] -> Int
+stockDeProducto [] v = 0
 stockDeProducto (x:xs) v | not (perteneceProducto v (x:xs)) =0
                          | perteneceProducto v (x:xs) = devuelveCantidad v (x:xs)
 
@@ -53,33 +55,39 @@ devuelveCantidad v (x:xs) | v==fst x =snd x
 
 --EJERCICIO 3:
 dineroEnStock :: [([Char],Int)] -> [([Char],Float)] -> Float
-dineroEnStock (x:xs) (y:ys) = multiplicacion (suma2doElementoI (x:xs)) (suma2doElementoF (devuelvePreciosStock (x:xs) (y:ys)))
+dineroEnStock [] [] = 0
+dineroEnStock [] (p:precios) = 0
+dineroEnStock (s:stock) (p:precios) = dineroPorProductosEnStock (s:stock) (devuelvePreciosEnStock (s:stock) (p:precios))
+
+--devuelve la lista de precios de los productos que sí estan en stock:
+devuelvePreciosEnStock :: [([Char],Int)] -> [([Char],Float)] -> [([Char],Float)] 
+devuelvePreciosEnStock [s] [p] | fst s ==fst p =[p]
+                               | otherwise =[]
+devuelvePreciosEnStock (s:stock) [p] | fst s ==fst p =[p]
+                                     | otherwise = devuelvePreciosEnStock (stock) [p]
+devuelvePreciosEnStock (s:stock) (p:precios) | fst s ==fst p =[p] ++ devuelvePreciosEnStock (s:stock) (precios)
+                                             | otherwise = devuelvePreciosEnStock (stock) [p] ++ devuelvePreciosEnStock (s:stock) (precios)
 
 multiplicacion :: Int -> Float -> Float
 multiplicacion n m = (fromIntegral n)*(m) 
 
-suma2doElementoI :: [([Char],Int)] -> Int
-suma2doElementoI [x] = snd x
-suma2doElementoI (x:xs) = snd x + suma2doElementoI (xs)
+--multiplicación de la cantidad del producto en stock por su precio:
+dineroPorProductoEnStock :: ([Char],Int) -> [([Char],Float)] -> Float
+dineroPorProductoEnStock s (p:precios) | fst s == fst p =multiplicacion (snd s) (snd p)
+                                       | otherwise = dineroPorProductoEnStock s (precios)
 
-suma2doElementoF :: [([Char],Float)] -> Float
-suma2doElementoF [x] = snd x
-suma2doElementoF (x:xs) = snd x + suma2doElementoF (xs)
-
-devuelvePreciosStock :: [([Char],Int)] -> [([Char],Float)] -> [([Char],Float)] 
-devuelvePreciosStock [x] [y] | fst y ==fst x =[y]
-                             | otherwise =[]
-devuelvePreciosStock (x:xs) [y] | fst y ==fst x =[y]
-                                | otherwise = devuelvePreciosStock (xs) [y]
-devuelvePreciosStock (x:xs) (y:ys) | fst y ==fst x =[y] ++ devuelvePreciosStock (x:xs) (ys)
-                                   | otherwise = devuelvePreciosStock (xs) [y] ++ devuelvePreciosStock (x:xs) (ys)
-
+--suma de las multiplicaciones de cada producto:
+dineroPorProductosEnStock :: [([Char],Int)] -> [([Char],Float)] -> Float
+dineroPorProductosEnStock [s] (p:precios) = dineroPorProductoEnStock s (p:precios)
+dineroPorProductosEnStock (s:stock) (p:precios) = dineroPorProductoEnStock s (p:precios) + dineroPorProductosEnStock (stock) (p:precios)
 
 
 
 
 --EJERCICIO 4:
 aplicarOferta :: [([Char],Int)] -> [([Char],Float)] -> [([Char],Float)]
+aplicarOferta [] (y:ys) = []
+aplicarOferta (x:xs) [] = []
 aplicarOferta (x:xs) [y] | stockDeProducto (x:xs) (fst y) > 10 = [(fst y, multiplicacion4 (snd y) (0.80))]
                          | stockDeProducto (x:xs) (fst y) <= 10 = [y]
 aplicarOferta (x:xs) (y:ys) | stockDeProducto (x:xs) (fst y) > 10 = [(fst y, multiplicacion4 (snd y) (0.80))] ++ aplicarOferta (x:xs) (ys)
